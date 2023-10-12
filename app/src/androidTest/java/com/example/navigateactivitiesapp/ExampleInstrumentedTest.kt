91a36e69f853fe5b8ea42918f5ca3b2cd17334c1
Aman Ahmed <aman.ahmed97@gmail.com>
2023-10-11 21:26:35 -0400

    added ITs

--- app/src/androidTest/java/com/example/navigateactivitiesapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/navigateactivitiesapp/ExampleInstrumentedTest.kt
@@ -1,5 +1,10 @@
 package com.example.navigateactivitiesapp
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -20,5 +25,41 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.navigateactivitiesapp", appContext.packageName)
+    }
+
+    @Test
+    fun flingToNavigateActivityUp() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageView3)).perform(ViewActions.swipeUp())
+        Thread.sleep(1000)
+        activityScenario.close()
+    }
+
+    @Test
+    fun flingToNavigateActivityDown() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageView3)).perform(ViewActions.swipeDown())
+        Thread.sleep(1000)
+        activityScenario.close()
+    }
+
+    @Test
+    fun flingToNavigateActivityRight() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageView3)).perform(ViewActions.swipeRight())
+        Thread.sleep(1000)
+        activityScenario.close()
+    }
+
+    @Test
+    fun flingToNavigateActivityLeft() {
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.imageView3)).perform(ViewActions.swipeLeft())
+        Thread.sleep(1000)
+        activityScenario.close()
     }
 }
